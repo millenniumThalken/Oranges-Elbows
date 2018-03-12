69a09b0f454c4a0687683150f8c2c00c73fed7d1
millenniumThalken <pthalken@gmail.com>
2018-03-12 15:46:49 -0500

    working on the google map

--- src/app/ggmaps/ggmaps.component.ts
+++ src/app/ggmaps/ggmaps.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GgmapsComponent implements OnInit {
     title: string = 'Areas We Serve';
+    zoom: number = 8;
     iowa_city_lat: number = 41.661127700;
     iowa_city_long: number = -91.530168300;
+    west_branch_lat: number = 41.6677297;
+    west_branch_long: number = -91.347328;
+    coralville_lat: number = 41.6978658;
+    coralville_long: number = -91.597952;
+    riverside_lat: number = 41.4829312;
+    riverside_long: number = -91.5725;
+
 
     constructor() { }
 
