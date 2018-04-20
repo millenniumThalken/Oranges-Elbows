af5b732984021ad122238a02feb7e54e8c81e3f8
millenniumThalken <patrick-thalken@uiowa.edu>
2018-04-20 15:05:53 -0500

    Desktop ready for deploy

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -13,10 +13,10 @@ export class CarouselComponent implements OnInit {
     //here I have created my own Image model and am using it here to populate an Object of type Image
     public images: Image[] = [
         { "title": "Detailed Services", "message": "Eco-friendly Cleaning That Keeps the Environment and you Healthy", "link": "/our-services", "url": "/assets/images/Lavender.jpg", "selected": false },
-        { "title": "A Commitment to Being Green ", "message": "Oranges&Elbows is 100% committed to your health and the environment, check out our testimonials", "link": "/#", "url": "/assets/images/kitchen_one.jpg", "selected": false },
-        { "title": "Contact Us", "message": "Free customized estimates, so contact us today!", "link": "/#", "url": "/assets/images/kitchen_two.jpg", "selected": false },
-        { "title": "Service with a Smile", "message": "Our crew is the best in the business", "link": "/#", "url": "/assets/images/VinegarAndLemons.jpg", "selected": false },
-        { "title": "Handmade Cleaning Products", "message": "utilizing HAWKEYE vodka, Go Hawks!", "link": "/#", "url": "/assets/images/YellowGlove.jpg", "selected": false }
+        { "title": "Proud to be Green ", "message": "Committed to your health and the environment, check out our testimonials", "link": "/testimonials", "url": "/assets/images/kitchen_one.jpg", "selected": false },
+        { "title": "Contact Us", "message": "Free customized estimates, so contact us today!", "link": "/contact", "url": "/assets/images/kitchen_two.jpg", "selected": false },
+        { "title": "Service with a Smile", "message": "Our crew is the best in the business", "link": "/meet-the-crew", "url": "/assets/images/VinegarAndLemons.jpg", "selected": false },
+        { "title": "Handmade Products", "message": "utilizing safe quality ingredients", "link": "/#", "url": "/assets/images/YellowGlove.jpg", "selected": false }
     ];
 
     ngOnInit() {
