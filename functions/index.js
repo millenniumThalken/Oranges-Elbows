c02ab974f9210fbbfbaaf69e5cfc02e480b3bfe2
millenniumThalken <patrick-thalken@uiowa.edu>
2018-06-03 14:29:58 -0500

    Contact form nightmare

--- functions/index.js
+++ functions/index.js
@@ -11,4 +11,29 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
 admin.initializeApp();
+
+exports.sendContactMessage = functions.database.ref('/messages/{pushKey}').onWrite(event => {
+    const snapshot = event.data;
+
+    if (snapshot.previous.val() || !snapshot.val().name) {
+        return;
+    }
+
+    const val = snapshot.val();
+
+    const mailOptions = {
+        to: 'orangesandelbows@gmail.com',
+        subject: `Information Request from ${val.name}`,
+        html: val.html
+    };
+
+    return mailTransport.sendMail(mailOptions).then(() => {
+        return console.log('Mail is sent');
+    });
+});
